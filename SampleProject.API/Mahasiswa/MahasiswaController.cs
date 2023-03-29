5a791d884f880d438df0b5b39385252b5740c4fb
hakuryuu112 <rifki.ramdani@students.amikom.ac.id>
2023-03-29 15:00:55 +0700

    Add project files.

---
+++ SampleProject.API/Mahasiswa/MahasiswaController.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleProject.API.Mahasiswa
+{
+    [Route("api/mahasiswa")]
+    [ApiController]
+    public class MahasiswaController
+    {
+        private readonly IMediator mediator;
+    }
+}
